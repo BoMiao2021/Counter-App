cba6095cee4127312ff6e5de8e8491fde8da9ba6
BoMiao2021 <bo.miao@nutanix.com>
2021-12-16 17:57:09 -0600

    fetch data by using axios

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,12 +1,13 @@
 import React, { Component } from "react";
+import axios from "axios";
 
 class Counter extends React.Component {
-    // do not need state in Counter Component anymore, the value should be passed from the prop
-    // directly to keep the sync
-//   state = {
-//     value: this.props.counter.value,
-//     // tags: ["tag1", "tag2", "tag3"],
-//   };
+  // do not need state in Counter Component anymore, the value should be passed from the prop
+  // directly to keep the sync
+  //   state = {
+  //     value: this.props.counter.value,
+  //     // tags: ["tag1", "tag2", "tag3"],
+  //   };
 
   //   constructor() {
   //     super();
@@ -14,10 +15,10 @@ class Counter extends React.Component {
   // }
 
   // use arrow function to inherite 'this'
-//   handleIncrement = (product) => {
-//     // can not use this.state.count++, instead should use the built-in function which is inherited from Component
-//     this.setState({ value: this.state.value + 1 });
-//   };
+  //   handleIncrement = (product) => {
+  //     // can not use this.state.count++, instead should use the built-in function which is inherited from Component
+  //     this.setState({ value: this.state.value + 1 });
+  //   };
 
   render() {
     // console.log("props", this.props);
@@ -37,6 +38,7 @@ class Counter extends React.Component {
         >
           Delete
         </button>
+        <button onClick={this.getDataFromAPI}>Get Data From Backend API</button>
         {/* <ul>
           {this.state.tags.map((tag) => (
             <li key={tag}>{tag}</li>
@@ -55,6 +57,24 @@ class Counter extends React.Component {
   formatCount() {
     const { value: count } = this.props.counter;
     return count === 0 ? "Zero" : count;
+  }
+
+  getDataFromAPI() {
+    const url =
+      "http://echo.jsontest.com/insert-key-here/insert-value-here/key/value";
+    axios
+      .get(url)
+      .then(function (response) {
+        const data = response.data;
+        let output = `retrieved data from url: ${url}\n`;
+        for (let k in data) {
+          output += `key is ${k}, value is ${data[k]}\n`;
+        }
+        alert(output);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
   }
 }
 
